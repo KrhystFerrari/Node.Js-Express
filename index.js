32cfc11b0ba655745927b6103f91c9fbed5bcf00
Krhystofferson Ferrari <krhys_ferrari@hotmail.com>
2022-09-24 12:18:38 -0300

    config+create validate API

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const userRoute = require('./routes/userRoute')
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+userRoute(app)
+
+app.get('/', (req, res) => res.send("Hello, World! By Express. "))
+app.listen(port, () => console.log('API rodando na porta 3000 '))
